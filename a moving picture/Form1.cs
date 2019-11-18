33c19eaa1d885b3cfb52ce69eb96381c3b128716
Luke Harrison <rc0037143@student.riversidecollege.ac.uk>
2019-11-18 15:45:10 +0000

    Add project files.

---
+++ a moving picture/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a_moving_picture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 100, pictureBox1.Location.Y - 40);
+        }
+    }
+}
